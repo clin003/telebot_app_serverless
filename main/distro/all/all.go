adcb4ff9c5d318f2476563b1d4a9126a3d20fe7d
白菜林 <noreply@3ae.cn>
2023-02-09 02:22:27 +0800

    v0.0.2 debug

--- main/distro/all/all.go
+++ main/distro/all/all.go
@@ -4,9 +4,7 @@ import (
 	_ "github.com/clin003/tgbot_app_dev/features/about"
 	_ "github.com/clin003/tgbot_app_dev/features/getid"
 
-	// _ "github.com/clin003/tgbot_app_dev/features/menu"
 	_ "github.com/clin003/tgbot_app_dev/features/ping"
-	_ "github.com/clin003/tgbot_app_dev/features/questionmarkreply"
 	_ "github.com/clin003/tgbot_app_dev/features/start"
 	_ "github.com/clin003/tgbot_app_dev/features/video"
 )
