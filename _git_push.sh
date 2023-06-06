82f0fcfb31eaad39f92a299bf1f92f5a1093ef6f
白菜林 <noreply@3ae.cn>
2023-06-06 16:03:23 +0800

    v0.1.5 debug

--- _git_push.sh
+++ _git_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.1.3
+VERSION=0.1.5
 APPNAME=telebot_app_serverless
 echo "package baicai" > ./baicai/version.go
 echo "const(APP_NAME = \"${APPNAME}\"" >> ./baicai/version.go
