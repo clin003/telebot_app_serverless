f77ec352cdfd6cea07d1d175e5de70927dcd7cc4
白菜林 <noreply@3ae.cn>
2023-06-30 01:51:03 +0800

    v0.1.13 debug

--- _git_push.sh
+++ _git_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.1.12
+VERSION=0.1.13
 APPNAME=telebot_app_serverless
 echo "package baicai" > ./baicai/version.go
 echo "const(APP_NAME = \"${APPNAME}\"" >> ./baicai/version.go
--- features/text/text.go
+++ features/text/text.go
@@ -9,43 +9,68 @@ import (
 )
 
 // Command: /start <PAYLOAD>
-func OnText(c tele.Context) error {
-	fmt.Println("OnText", 0)
+func OnChannelLinkGroup(c tele.Context) error {
+	if len(replyText) <= 0 {
+		return nil
+	}
+	fmt.Println("OnChannelLinkGroup", 0)
 	if c.Message().Private() ||
 		c.Message().FromChannel() ||
 		c.Message().IsReply() {
 		return nil
 	}
-	fmt.Println("OnText", 1)
+	fmt.Println("OnChannelLinkGroup", 1)
 	if !(c.Message().OriginalChat != nil) || !(c.Message().SenderChat != nil) {
 		return nil
 	}
-	fmt.Println("OnText", 2)
+	fmt.Println("OnChannelLinkGroup", 2)
 	if c.Message().OriginalChat.Type != tele.ChatChannel ||
 		c.Message().SenderChat.Type != tele.ChatChannel ||
 		!c.Message().FromGroup() {
 		return nil
 	}
-	fmt.Println("OnText", 3)
-	menu := &tele.ReplyMarkup{ResizeKeyboard: true}
-	btn1 := menu.URL("薅羊毛📦", "https://t.me/haowu_push")
-	btn2 := menu.URL("值得买🔥", "https://t.me/haowu_dw")
-	menu.Reply(
-		menu.Row(btn1),
-		menu.Row(btn2),
-	)
+	fmt.Println("OnChannelLinkGroup", 3)
+	// menu := &tele.ReplyMarkup{ResizeKeyboard: true}
+	// btn1 := menu.URL("薅羊毛📦", "https://t.me/haowu_push")
+	// btn2 := menu.URL("值得买🔥", "https://t.me/haowu_dw")
+	// menu.Reply(
+	// 	menu.Row(btn1),
+	// 	menu.Row(btn2),
+	// )
 	selector := &tele.ReplyMarkup{}
-	btnPrev := selector.URL("薅羊毛📦", "https://t.me/haowu_push")
-	btnNext := selector.URL("值得买🔥", "https://t.me/haowu_dw")
+	if len(replyBtn1Text) <= 0 {
+		replyBtn1Text = "薅羊毛📦"
+	}
+	if len(replyBtn1Url) <= 0 {
+		replyBtn1Url = "https://t.me/haowu_push"
+	}
+	if len(replyBtn2Text) <= 0 {
+		replyBtn2Text = "值得买🔥"
+	}
+	if len(replyBtn2Url) <= 0 {
+		replyBtn2Url = "https://t.me/haowu_dw"
+	}
+	btnPrev := selector.URL(replyBtn1Text, replyBtn1Url)
+	btnNext := selector.URL(replyBtn2Text, replyBtn2Url)
 	selector.Inline(
-		selector.Row(btnPrev, btnNext),
+		selector.Row(
+			btnPrev,
+			btnNext,
+		),
 	)
-	c.Reply("评论区请友好👬发言selector", selector)
-	c.Reply("评论区请友好👬发言menu", menu)
-	menu.ResizeKeyboard = false
-	return c.Reply("评论区请友好👬发言", menu)
+	// c.Reply("评论区请友好👬发言selector", selector)
+	// c.Reply("评论区请友好👬发言menu", menu)
+	return c.Reply(replyText, selector)
 }
 
+var replyText, replyBtn1Text, replyBtn1Url, replyBtn2Text, replyBtn1Url string
+
 func init() {
-	features.RegisterFeature(tele.OnText, OnText)
+	features.RegisterFeature(tele.OnText, OnChannelLinkGroup)
+
+	replyText = os.Getenv("BAICAI_BOT_REPLY_TEXT")
+	replyBtn1Text = os.Getenv("BAICAI_BOT_REPLY_BTN1_TEXT")
+	replyBtn1Url = os.Getenv("BAICAI_BOT_REPLY_BTN1_URL")
+	replyBtn2Text = os.Getenv("BAICAI_BOT_REPLY_BTN2_TEXT")
+	replyBtn1Url = os.Getenv("BAICAI_BOT_REPLY_BTN2_URL")
 }
